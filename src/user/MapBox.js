2e084dccdcf5343deb5cd66d469c14ad413ac857
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-06-16 18:23:30 +0900

    주석 해제

--- src/user/MapBox.js
+++ src/user/MapBox.js
@@ -36,37 +36,37 @@ const MapBox = () => {
 	const [metaTypes, setMetaTypes] = useState([]);
 	const [available, setAvailable] = useState(false);
 
-	const handleReload = () => {
-		if (navigator.geolocation) {
-			// GeoLocation을 이용해서 접속 위치를 얻어옵니다
-			navigator.geolocation.getCurrentPosition(
-				position => {
-					setState(prev => ({
-						...prev,
-						center: {
-							lat: position.coords.latitude, // 위도
-							lng: position.coords.longitude, // 경도
-						},
-						isLoading: false,
-					}));
-				},
-				err => {
-					setState(prev => ({
-						...prev,
-						errMsg: err.message,
-						isLoading: false,
-					}));
-				},
-			);
-		} else {
-			// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-			setState(prev => ({
-				...prev,
-				errMsg: 'geolocation을 사용할수 없어요..',
-				isLoading: false,
-			}));
-		}
-	};
+	// const handleReload = () => {
+	// 	if (navigator.geolocation) {
+	// 		// GeoLocation을 이용해서 접속 위치를 얻어옵니다
+	// 		navigator.geolocation.getCurrentPosition(
+	// 			position => {
+	// 				setState(prev => ({
+	// 					...prev,
+	// 					center: {
+	// 						lat: position.coords.latitude, // 위도
+	// 						lng: position.coords.longitude, // 경도
+	// 					},
+	// 					isLoading: false,
+	// 				}));
+	// 			},
+	// 			err => {
+	// 				setState(prev => ({
+	// 					...prev,
+	// 					errMsg: err.message,
+	// 					isLoading: false,
+	// 				}));
+	// 			},
+	// 		);
+	// 	} else {
+	// 		// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+	// 		setState(prev => ({
+	// 			...prev,
+	// 			errMsg: 'geolocation을 사용할수 없어요..',
+	// 			isLoading: false,
+	// 		}));
+	// 	}
+	// };
 
 	// useEffect(() => {
 	// 	if (navigator.geolocation) {
@@ -133,9 +133,9 @@ const MapBox = () => {
 	};
 
 	// 현위치로 새로고침
-	// const handleReload = () => {
-	// 	window.location.reload();
-	// };
+	const handleReload = () => {
+		window.location.reload();
+	};
 
 	// const handleTest = () => {
 	// 	setTest(true);
