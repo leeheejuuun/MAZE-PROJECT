8b3d80951594c36377392ce0764e8710317c097f
leeheejuuun <94230809+leeheejuuun@users.noreply.github.com>
2022-06-17 09:48:02 +0900

    지오로케이션 주석해제

--- src/user/MapBox.js
+++ src/user/MapBox.js
@@ -15,7 +15,7 @@ const MapBox = () => {
 
 	const [state, setState] = useState({
 		// 지도의 초기 위치
-		center: { lat: 37.5738319, lng: 127.1946859 },
+		center: { lat: null, lng: null },
 		// { lat: 37.476086, lng: 127.123543 }
 
 		// 지도 위치 변경시 panto를 이용할지(부드럽게 이동)
@@ -96,37 +96,37 @@ const MapBox = () => {
 
 	// const [test, setTest] = useState(false);
 
-	// useEffect(() => {
-	// 	if (navigator.geolocation) {
-	// 		// GeoLocation을 이용해서 접속 위치를 얻어옵니다
-	// 		navigator.geolocation.getCurrentPosition(
-	// 			position => {
-	// 				setState(prev => ({
-	// 					...prev,
-	// 					center: {
-	// 						lat: position.coords.latitude, // 위도
-	// 						lng: position.coords.longitude, // 경도
-	// 					},
-	// 					isLoading: false,
-	// 				}));
-	// 			},
-	// 			err => {
-	// 				setState(prev => ({
-	// 					...prev,
-	// 					errMsg: err.message,
-	// 					isLoading: false,
-	// 				}));
-	// 			},
-	// 		);
-	// 	} else {
-	// 		// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-	// 		setState(prev => ({
-	// 			...prev,
-	// 			errMsg: 'geolocation을 사용할수 없어요..',
-	// 			isLoading: false,
-	// 		}));
-	// 	}
-	// }, []);
+	useEffect(() => {
+		if (navigator.geolocation) {
+			// GeoLocation을 이용해서 접속 위치를 얻어옵니다
+			navigator.geolocation.getCurrentPosition(
+				position => {
+					setState(prev => ({
+						...prev,
+						center: {
+							lat: position.coords.latitude, // 위도
+							lng: position.coords.longitude, // 경도
+						},
+						isLoading: false,
+					}));
+				},
+				err => {
+					setState(prev => ({
+						...prev,
+						errMsg: err.message,
+						isLoading: false,
+					}));
+				},
+			);
+		} else {
+			// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+			setState(prev => ({
+				...prev,
+				errMsg: 'geolocation을 사용할수 없어요..',
+				isLoading: false,
+			}));
+		}
+	}, []);
 
 	// console.log(state);
 	// console.log(area);
@@ -216,7 +216,7 @@ const MapBox = () => {
 		// }
 
 		fetch(
-			`${API.CAFE}${new URLSearchParams({
+			`${API.CAFE}?${new URLSearchParams({
 				...area,
 			})}`,
 		)
